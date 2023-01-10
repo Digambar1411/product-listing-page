f4f3993c9cc4c87287eee19847807fdb681cfb5e
Digambar <deshawaldigambar@gmail.com>
2023-01-10 23:02:29 +0530

    Added products

---
+++ src/Database/products.js
@@ -0,0 +1,115 @@
+import { v4 as uuid } from "uuid";
+
+export const products = [
+	{
+		id: uuid(),
+		name: "Men Ankle Length",
+		brand: "Kamini",
+		size: "M",
+		ideal_for: "Men",
+		price: "530",
+		image: ["/assets/1.webp"],
+		
+	},
+
+	{
+		id: uuid(),
+		name: "Men T-shirt",
+		brand: "Roadster",
+		size: "L",
+		ideal_for: "Men",
+		price: "600",
+		image: ["/assets/2.webp"],
+		
+	},
+
+	{
+		id: uuid(),
+		name: "Women Printed Anarkali Kurta",
+		brand: "FastColors",
+		size: "S",
+		ideal_for: "Women",
+		price: "499",
+		image: ["/assets/3.webp"],
+	},
+
+	{
+		id: uuid(),
+		name: "Men Fullsleves Shirt",
+		brand: "FastColors",
+		size: "M",
+		ideal_for: "Women",
+		price: "599",
+		image: ["/assets/4.webp"],
+	},
+
+	{
+		id: uuid(),
+		name: "Men Casual Wear",
+		brand: "Adidas",
+		size: "L",
+		ideal_for: "Men",
+		price: "399",
+		image: ["/assets/5.webp"],
+	},
+
+	{
+		id: uuid(),
+		name: "Women Anarkali Kurta",
+		brand: "Nike",
+		size: "S",
+		ideal_for: "Women",
+		price: "449",
+		image: ["/assets/6.webp"],
+	},
+
+	{
+		id: uuid(),
+		name: "Men Sweatshirt",
+		brand: "Roadster",
+		size: "XL",
+		ideal_for: "Men",
+		price: "799",
+		image: ["/assets/Men_sweatshirt.webp"],
+	},
+
+	{
+		id: uuid(),
+		name: "Men Solid Cotton Kurtas",
+		brand: "Roadster",
+		size: "M",
+		ideal_for: "Men",
+		price: "999",
+		image: ["/assets/Men-Solid-Cotton-Kurtas.webp"],
+	},
+
+	{
+		id: uuid(),
+		name: "Women Unstitched Cotton Salwar",
+		brand: "Roadster",
+		size: "M",
+		ideal_for: "Women",
+		price: "999",
+		image: ["/assets/Women-Unstitched-Cotton-Salwar.webp"],
+	},
+
+	{
+		id: uuid(),
+		name: "Self Design Assam Silk Saree",
+		brand: "Nixyo",
+		size: "M",
+		ideal_for: "Women",
+		price: "349",
+		image: ["/assets/Self-Design-Assam-Silk-Saree.webp"],
+	},
+
+	{
+		id: uuid(),
+		name: "Printed Geometric Cotton Silk Saree",
+		brand: "Samah",
+		size: "M",
+		ideal_for: "Women",
+		price: "239",
+		image: ["/assets/Printed-Geometric-Cotton-Silk-Saree.webp"],
+	},
+];
