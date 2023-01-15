74682632e0d3c335302d85b4a6ebad746ad25ee2
Digambar <deshawaldigambar@gmail.com>
2023-01-15 14:19:52 +0530

    product screen and app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import { Navbar, Aside} from './components';
+import ProductsScreen from './screen/ProductsScreen';
+import { useState } from 'react';
 
 function App() {
+
+  const [mobileAside, setMobileAside]= useState(false);
+
   return (
-   <></>
+   <div className='app flex-col'>
+      <Navbar />
+      {mobileAside && <Aside mobileAside={mobileAside} />}
+      <div className="section-down">
+        <div className='aside-container'><Aside mobileAside={mobileAside}/></div>
+        <ProductsScreen mobileAside={mobileAside} setMobileAside={setMobileAside} />
+      </div>
+   </div>
   );
 }
 
